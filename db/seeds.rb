bce454c9e2581bb417706f795aaa3a04c65e60e2
brokenSensor <jonimen95@yandex.ru>
2022-04-03 14:14:21 +0800

    Migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+projects = Project.create([{title: "Семья"}, {title: "Работа"}, {title: "Прочее"}])
+
+Todo.create(project: projects[0], text: "Купить молоко", isCompleted: false)
+Todo.create(project: projects[0], text: "Заменить масло в двигателе до 23 апреля", isCompleted: false)
+Todo.create(project: projects[0], text: "Отправить письмо бабушке", isCompleted: true)
+Todo.create(project: projects[0], text: "Заплатить за квартиру", isCompleted: false)
+Todo.create(project: projects[0], text: "Забрать обувь из ремонта", isCompleted: false)
+
+Todo.create(project: projects[1], text: "Позвонить заказчику", isCompleted: true)
+Todo.create(project: projects[1], text: "Отправить документы", isCompleted: true)
+Todo.create(project: projects[1], text: "Заполнить отчет", isCompleted: false)
+
+Todo.create(project: projects[2], text: "Позвонить другу", isCompleted: false)
+Todo.create(project: projects[2], text: "Подготовиться к поездке", isCompleted: false)
